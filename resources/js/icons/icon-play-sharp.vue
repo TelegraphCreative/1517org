d2c27d650f91a6d6a94bb91d7c210210bd7fe51b
jjjohn <johnmueller@fastmail.com>
2019-04-30 12:51:06 -0600

    video str te and icons

---
+++ resources/js/icons/icon-play-sharp.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="icon | " :class="classes">
+    <svg viewBox="0 0 12.6 16" xmlns="http://www.w3.org/2000/svg" >
+      <polygon fill="currentColor" points="0,0 0,16 12.6,8 " />
+    </svg>
+  </span>
+</template>
+<script>
+export default {
+  props: ["classes"]
+};
+</script>
