5cbb1246405ed6457fd14a22e062a3a182994e77
Lindsey Bradford <lindseyb@wearetelegraph.com>
2019-05-08 09:25:29 -0500

    Speaker layout, form, datepicker component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,18 +2,19 @@
 window.Vue = require('vue')
 
 // Imports
-import lazysizes from "lazysizes";
-import site_menu from "./components/SiteMenu";
+import lazysizes from 'lazysizes'
+import site_menu from './components/SiteMenu'
 import search_menu from './components/SearchMenu'
-import select_box from "./components/SelectBox";
-import video_player_full from "./components/VideoPlayerFull";
-import video_content_card from "./components/VideoContentCard";
-import podcast_player_compact from "./components/PodcastPlayerCompact";
-import podcast_player_full from "./components/PodcastPlayerFull";
-import seconds_to_time_filter from "./filters/SecondsToTime";
-import print from "./components/Print";
-import share from "./components/Share";
-import dropcap from "./components/DropCap";
+import select_box from './components/SelectBox'
+import date_picker from './components/DatePicker'
+import video_player_full from './components/VideoPlayerFull'
+import video_content_card from './components/VideoContentCard'
+import podcast_player_compact from './components/PodcastPlayerCompact'
+import podcast_player_full from './components/PodcastPlayerFull'
+import seconds_to_time_filter from './filters/SecondsToTime'
+import print from './components/Print'
+import share from './components/Share'
+import dropcap from './components/DropCap'
 
 // Vue App
 const app = new Vue({
@@ -23,16 +24,15 @@ const app = new Vue({
         site_menu,
         search_menu,
         select_box,
+        date_picker,
         video_player_full,
         video_content_card,
         podcast_player_compact,
         podcast_player_full,
         print,
-        share
-    }
-});
-
-
+        share,
+    },
+})
 
 // Other Initializations
-dropcap();
+dropcap()
