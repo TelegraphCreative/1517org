910c6c40c62eb2b25c15a37332c1d5f36bda2ca2
jjjohn <johnmueller@fastmail.com>
2019-05-15 13:19:27 -0600

    search filtering

--- resources/js/components/SelectBox.vue
+++ resources/js/components/SelectBox.vue
@@ -1,45 +1,44 @@
 <template>
-  <select
-    class="selectdrop">
-    <slot />
-  </select>
-  <!-- v-model="selected" -->
+	<select v-model="selected" class="selectdrop">
+		<slot/>
+	</select>
 </template>
 <script>
-  import Choices from 'choices.js'
-  export default {
+import Choices from 'choices.js'
+import Url from 'domurl'
+export default {
     props: {
-      value: {
-        type: String,
-        required: false
-      },
-      opt: {
-        type: Object,
-        required: false,
-        default() {
-          return {
-          }
-        }
-      }
+        value: {
+            type: String,
+            required: false,
+        },
+        opt: {
+            type: Object,
+            required: false,
+            default() {
+                return {}
+            },
+        },
     },
     computed: {
         selected: {
             get() {
-            return this.value
+                return this.value
             },
             set(value) {
-            this.$emit('input', value)
-            }
-        }
+                const url = new Url
+                url.query[this.opt.name] = value
+                window.location = url
+            },
+        },
     },
     mounted() {
         const _this = this
         const selectDrop = _this.$el
-      
-        window.addEventListener('load', function () {
 
+        window.addEventListener('load', function() {
             // Handle Placeholder
-            if (typeof (_this.opt.prependPlaceholder) !== 'undefined') {
+            if (typeof _this.opt.prependPlaceholder !== 'undefined') {
                 var placeholderText = _this.opt.prependPlaceholder
             } else {
                 var placeholderText = ''
@@ -53,54 +52,52 @@
                     containerOuter: 'choices ' + _this.opt.variant,
                 },
                 shouldSort: false,
-                callbackOnCreateTemplates: function (template) {
+                callbackOnCreateTemplates: function(template) {
                     return {
-                    item: (classNames, data) => {
-                        return template(`
-                        <div class="${classNames.item} ${data.highlighted ? classNames.highlightedState : classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" ${data.active ? 'aria-selected="true"' : ''} ${data.disabled ? 'aria-disabled="true"' : ''}>
+                        item: (classNames, data) => {
+                            return template(`
+                        <div class="${classNames.item} ${
+                                data.highlighted
+                                    ? classNames.highlightedState
+                                    : classNames.itemSelectable
+                            }" data-item data-id="${data.id}" data-value="${
+                                data.value
+                            }" ${data.active ? 'aria-selected="true"' : ''} ${
+                                data.disabled ? 'aria-disabled="true"' : ''
+                            }>
                             ${placeholderText} ${data.label}
                         </div>
                         `)
-                    },
-
+                        },
                     }
-                }
+                },
             })
 
             // Handle Scroll Anchors
-            if (typeof (_this.opt.scrollAnchors) !== 'undefined') {
-
-                selectDrop.addEventListener('change', function (event) {
-
+            if (typeof _this.opt.scrollAnchors !== 'undefined') {
+                selectDrop.addEventListener('change', function(event) {
                     // Scroll
                     const anchor = choices.getValue(true),
-                    anchor_element = document.getElementById(anchor),
-                    offset = 0
+                        anchor_element = document.getElementById(anchor),
+                        offset = 0
 
                     window.scroll({
-                    behavior: 'smooth',
-                    left: 0,
-                    top: anchor_element.offsetTop - offset
+                        behavior: 'smooth',
+                        left: 0,
+                        top: anchor_element.offsetTop - offset,
                     })
-
                 })
             }
 
             // Handle Links
-            if (typeof (_this.opt.linkList) !== 'undefined') {
-                
-                selectDrop.addEventListener('change', function (event) {
-
+            if (typeof _this.opt.linkList !== 'undefined') {
+                selectDrop.addEventListener('change', function(event) {
                     // Location
-                    const anchor = choices.getValue(true);
-                    window.location = anchor;
-
+                    const anchor = choices.getValue(true)
+                    window.location = anchor
                 })
-
             }
-
-
         })
-    }
-  }
+    },
+}
 </script>
