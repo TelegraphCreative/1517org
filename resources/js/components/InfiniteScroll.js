507034073f8d9afc4b2993ef5c9b161bac08413a
jjjohn <johnmueller@fastmail.com>
2019-06-12 15:38:04 -0600

    es6 linting

--- resources/js/components/InfiniteScroll.js
+++ resources/js/components/InfiniteScroll.js
@@ -1,11 +1,10 @@
 import InfiniteScroll from 'infinite-scroll'
 
 export default function () {
-    console.log('initializing Infinite Scroll')
-    var pagination = document.querySelector('.pagination__next')
-    var elem = document.querySelector('.page-entries')
+    const pagination = document.querySelector('.pagination__next')
+    const elem = document.querySelector('.page-entries')
     if (pagination) {
-        var infScroll = new InfiniteScroll(elem, {
+        const infScroll = new InfiniteScroll(elem, {
             // options
             path: '.pagination__next',
             append: '.content-card',
